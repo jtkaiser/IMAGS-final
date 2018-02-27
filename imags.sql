aba94f37e057de0e62b68c0260cc4dce5d65833a
atwalder <atwalder@users.noreply.github.com>
2018-02-27 09:23:16 -0800

    Update imags.sql
    hopefully works now

--- imags.sql
+++ imags.sql
@@ -1,46 +1,46 @@
 create table if not exists Songs (
-	URI		varchar2(50) primary key,
+	URI varchar(50) primary key,
 	Acousticness float(5),
-	AnalysisURL varchar2(100),
+	AnalysisURL varchar(100),
 	Danceability float(5),
-	DurationMS number(10),
+	DurationMS int(10),
 	Energy float(5),
-	SongID varchar2(50),
+	SongID varchar(50),
 	Instrumentalness float(5),
-	SongKey number(1),
+	SongKey int(1),
 	Liveness float(5),
 	Loudness float(5),
-	SongMode number(1),
+	SongMode int(1),
 	Speechiness float(5),
-	Tempo			float(5),
-	TimeSignature number(5),
-	TrackHref varchar2(100),
+	Tempo float(5),
+	TimeSignature int(5),
+	TrackHref varchar(100),
 	Valence float(5),
-	Progressions varchar2(50),
+	Progressions varchar(50),
 );
 
 
 create table if not exists Sessions (
-	SessionID		char(36) primary key,
-	PatientID		varchar2(50),
-	SongURI 	varchar2(50),
-	BreakMED		varchar2(50),
-	SessionDURATION		datetime,
+	SessionID char(36) primary key,
+	PatientID varchar(50),
+	SongURI varchar(50),
+	BreakMED varchar(50),
+	SessionDURATION	datetime,
 	foreign key (SongURI) references Songs(URI)
 );
 
 
 create table if not exists PainLog (
-	SessionID		char(36),
-	SessionTime			datetime,
-	PainLVL			number(2),
+	SessionID char(36),
+	SessionTime datetime,
+	PainLVL int(2),
 	foreign key (SessionID) references Sessions(SessionID)
 );
 
 
 
 create table if not exists Patients (
-	PatientID		varchar2(50) primary key,
+	PatientID varchar(50) primary key,
 	foreign key (PatientID) references Sessions(PatientID)
 );
 
